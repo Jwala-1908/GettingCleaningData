2859a77508b12ecde7e6aab8e682a49f0ccd96e6
Prajjwal V <prajjwal1908@gmail.com>
2020-07-01 21:40:15 +0530

    updated Rscript

--- run_analysis.R
+++ run_analysis.R
@@ -20,6 +20,7 @@ fin_y <- fin_X[2]
 colnames(fin_y) <- c("Activity")
 fin_subject <- fin_X[1]
 colnames(fin_subject) <- c("SubjectID")
+combinedData <- fin_X
 fin_X <- fin_X[3:dim(fin_X)[2]]
 
 remove(Xtrain,Xtest,ytrain,ytest,fin_train,fin_test,subject_test,subject_train)
@@ -28,7 +29,8 @@ remove(Xtrain,Xtest,ytrain,ytest,fin_train,fin_test,subject_test,subject_train)
 ## STANDARD DEVIATION FOR EACH MEASUREMENT.
 feat <- read.table("UCI HAR Dataset/features.txt")
 colnames(fin_X) <- feat$V2
-findMeanStd <- fin_X[grepl("-mean()-", feat$V2, fixed = TRUE)|
+colnames(combinedData)<- c("SubjectID","Activity",feat$V2)
+findMeanStd <- combinedData[grepl("-mean()-", feat$V2, fixed = TRUE)|
                                grepl("-std()-", feat$V2, fixed =TRUE)]
 
 remove(feat)
@@ -41,14 +43,15 @@ labData <- read.table("UCI HAR Dataset/activity_labels.txt")
 colnames(labData) <- c("Code","Label")
 # Find and replace each activity code with the corresponding label
 for (i in 1:dim(fin_y)[1]) {
-    fin_y$V1[i] = labData$Label[which(labData$Code==fin_y$V1[i])]
+    combinedData$Activity[i] =  labData$Label[which(labData$Code==combinedData$Activity[i])]
+    fin_y$Activity[i] = labData$Label[which(labData$Code==fin_y$Activity[i])]
 }
 
-remove(labData)
+remove(i)
 
 ## 4. APPROPRIATELY LABELS THE DATA SET WITH
 ## DESCRIPTIVE VARIABLE NAMES
-z <- colnames(fin_X)
+z <- colnames(findMeanStd)
 expandName<- function(x){
     if (startsWith(x,"t")){
         x<-sub("t","Time",x)
@@ -61,13 +64,13 @@ expandName<- function(x){
     }
 }
 z <- sapply(z,expandName)
-colnames(fin_X) <- z
+colnames(findMeanStd) <- z
 remove(z)
 
 ## 5. FROM THE DATA SET IN STEP 4, CREATE A SECOND, INDEPENDENT
 ## TIDY DATASET WITH THE AVERAGE OF EACH VARIABLE FOR EACH
 ## ACTIVITY AND EACH SUBJECT.
-combined_fin <- cbind(fin_subject, fin_y, fin_X)
+#combined_fin <- cbind(fin_subject, fin_y, fin_X)
 newGen <- function(x, subj, act){
     z <- subset(x, x[1]==subj)
     z <- subset(z, z[2] == ac)
@@ -76,11 +79,15 @@ newGen <- function(x, subj, act){
 }
 
 new_fin <- data.frame()
-for (subj in unique(combined_fin$SubjectID)){
-    for (ac in unique(combined_fin$Activity)){
-        tmp <- c(subj, ac, newGen(combined_fin, subj, act))
+for (subj in unique(findMeanStd$SubjectID)){
+    for (ac in unique(findMeanStd$Activity)){
+        tmp <- c(subj, ac, newGen(findMeanStd, subj, act))
         new_fin <- rbind(new_fin, tmp)
     }
 }
+for (i in 1:dim(findMeanStd)[1]) {
+    findMeanStd$Activity[i] =  labData$Label[which(labData$Code==findMeanStd$Activity[i])]
+}
+colnames(new_fin) <- c(colnames(findMeanStd[1:2]), paste0("Average_Of_",colnames(findMeanStd)[3:dim(findMeanStd)[2]]))
 
-colnames(new_fin) <- c(colnames(combined_fin[1:2]), paste0("Average_Of_",colnames(combined_fin)[3:dim(combined_fin)[2]]))
+write.table(new_fin,file="tidyData.txt")
