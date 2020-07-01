e00ba64afc320fdb362a59a7602198cac875d141
Prajjwal V <prajjwal1908@gmail.com>
2020-07-01 19:15:38 +0530

    adding Rscript

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Getting and Cleaning Data Course Project
+
+This is the README file for the Course Project of the Course:
+GETTING AND CLEANING DATA
+
+The R_script run_analysis.R performs the following:
+1. Merges the training and the test sets to create one data set. 
+	fin_X contains the 'X's merged from X_test.txt and X_train.txt files
+	fin_y contains the 'y's merged from y_test.txt and y_train.txt files
+	fin_subject contains the subject IDs merged from subject_test.txt and subject_train.txt files
+
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+	findMeanStd contains the subsetted data containing only required features.
+
+3. Uses descriptive activity names to name the activities in the data set
+	The codes (1,2,3,4,5,6) have been replaced by the activity they represent
+
+4. Appropriately labels the data set with descriptive variable names.
+	Features beginning with 't' have been expanded by replacing 't' with 'Time'
+	Likewise, 'f' has been changed to 'Frequency'
+	
+5. From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+	The new dataset is stored as 'new_fin'
+	
---
+++ run_analysis.R
@@ -0,0 +1,86 @@
+## 1. MERGE THE TRAINING AND TEST SETS TO CREATE ONE DATA SET
+# Reading activity labels
+
+# Read train and test data
+Xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+ytrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+Xtest <- read.table("UCI HAR Dataset/test/X_test.txt")
+ytest <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# Join training and tets Xs and ys
+fin_train <- cbind(subject_train, ytrain, Xtrain)
+fin_test <- cbind(subject_test, ytest, Xtest)
+
+# Combine training and test sets
+fin_X <- rbind(fin_train,fin_test)
+fin_X <- fin_X[order(fin_X$V2),]
+fin_y <- fin_X[2]
+colnames(fin_y) <- c("Activity")
+fin_subject <- fin_X[1]
+colnames(fin_subject) <- c("SubjectID")
+fin_X <- fin_X[3:dim(fin_X)[2]]
+
+remove(Xtrain,Xtest,ytrain,ytest,fin_train,fin_test,subject_test,subject_train)
+
+## 2. EXTRACTS ONLY THE MEASUREMENTS ON THE MEAN AND
+## STANDARD DEVIATION FOR EACH MEASUREMENT.
+feat <- read.table("UCI HAR Dataset/features.txt")
+colnames(fin_X) <- feat$V2
+findMeanStd <- fin_X[grepl("-mean()-", feat$V2, fixed = TRUE)|
+                               grepl("-std()-", feat$V2, fixed =TRUE)]
+
+remove(feat)
+
+## 3. USES DESCRIPTIVE ACTIVITY NAMES TO NAME THE
+## ACTIVITIES IN THE DATA SET
+# Read the activity_labels data
+labData <- read.table("UCI HAR Dataset/activity_labels.txt")
+# Set column names
+colnames(labData) <- c("Code","Label")
+# Find and replace each activity code with the corresponding label
+for (i in 1:dim(fin_y)[1]) {
+    fin_y$V1[i] = labData$Label[which(labData$Code==fin_y$V1[i])]
+}
+
+remove(labData)
+
+## 4. APPROPRIATELY LABELS THE DATA SET WITH
+## DESCRIPTIVE VARIABLE NAMES
+z <- colnames(fin_X)
+expandName<- function(x){
+    if (startsWith(x,"t")){
+        x<-sub("t","Time",x)
+    }
+    else if (startsWith(x,"f")){
+        x<-sub("f","Frequency", x)
+    }
+    else {
+        x <- sub("angle","Angle",x)
+    }
+}
+z <- sapply(z,expandName)
+colnames(fin_X) <- z
+remove(z)
+
+## 5. FROM THE DATA SET IN STEP 4, CREATE A SECOND, INDEPENDENT
+## TIDY DATASET WITH THE AVERAGE OF EACH VARIABLE FOR EACH
+## ACTIVITY AND EACH SUBJECT.
+combined_fin <- cbind(fin_subject, fin_y, fin_X)
+newGen <- function(x, subj, act){
+    z <- subset(x, x[1]==subj)
+    z <- subset(z, z[2] == ac)
+    meanstd <- colMeans(z[3:dim(z)[2]])
+    return(meanstd)
+}
+
+new_fin <- data.frame()
+for (subj in unique(combined_fin$SubjectID)){
+    for (ac in unique(combined_fin$Activity)){
+        tmp <- c(subj, ac, newGen(combined_fin, subj, act))
+        new_fin <- rbind(new_fin, tmp)
+    }
+}
+
+colnames(new_fin) <- c(colnames(combined_fin[1:2]), paste0("Average_Of_",colnames(combined_fin)[3:dim(combined_fin)[2]]))
